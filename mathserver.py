a295dd41cd76fd6ef4fa928da06103153e6406b5
devv1002 <devmalik1002@gmail.com>
2026-07-25 13:50:44 +0530

    Initial commit

--- mathserver.py
+++ mathserver.py
@@ -14,5 +14,8 @@ def multiply(a:int,b:int) -> int:
     """Multiply two numbers"""
     return a * b
 
+# The transport="stdio" argument tells the server to:
+# Use standard input/output (stdin and stdout) to receive and respond to tool function calls
+
 if __name__ == "__main__":
     mcp.run(transport="stdio")
